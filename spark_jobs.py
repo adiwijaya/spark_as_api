1633fa464cb5c14410c3aecb4235e763034d3806
DESKTOP-5PHUL53\adiwi <adi@datalabs.id>
2018-11-19 12:26:30 +0700

    Fix spark code

--- spark_jobs.py
+++ spark_jobs.py
@@ -18,7 +18,9 @@ def count_length(string):
 
 def read_table(table_name):
     import pandas as pd
-    result = spark.sql("SELECT * FROM %s"%table_name)
+    from spark import get_spark
+    spark_session = get_spark()
+    result = spark_session.sql("SELECT * FROM %s"%table_name)
     pandas_df = df.toPandas()
     json_result = pandas_df.to_json
     return json_result
